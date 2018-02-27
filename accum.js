035ffdfe5a34151c9a57fb896eb39e95699902f6
MohammadAlRifai <mohammad94sar@gmail.com>
2018-02-27 20:50:51 +0200

    accum_

--- accum.js
+++ accum.js
@@ -6,19 +6,17 @@
 
     function accum(input){
     	var str = "";
-    	var input = input;
-        var aa ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var a ="abcdefghijklmnopqrstuvwxyz";
-        for (var j = 0; j < j.length; j++) {
-        for (var x = 0; x < aa.length; x++) {
-        for (var i = 0; i < a.length; i++) {
-        	if(input[j] === a[i]){
-        		return str + aa[x]; 
+        for (var i = 0; i < input.length; i++) {
+        str = str + input[i].toUpperCase();
+        rep = i + 1;
+        for (var x = 0; x < rep; x++) {
+
+        	str = str + input[i].toLowerCase();
         	}
-        	if (input[j] === aa[x]){
-        		return str + a[i];
-        	}
+        	str = str + "_"
+        	
     }
-	}
-	}
-	}
+        		return str.slice(0,-1)
+    }
+	
+	
